b2dcc22539ffc760d57d369842e335a36070776e
TessaRidderikhoff <tessaridderikhoff@gmail.com>
2018-10-16 18:07:51 +0200

    plot in python

---
+++ evaluate.java
@@ -0,0 +1,8 @@
+public class RunTest
+ {
+    public static void main(String[] args)
+      {
+          for(int i=1; i<=10; i++)
+              player70.main(args); 
+      }
+ }
--- player70.java
+++ player70.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.*;
+// import java.io.PrintWriter;
+// import java.io.File;
+
 // import org.apache.commons.lang.ArrayUtils;
 
 
@@ -16,9 +19,9 @@ public class player70 implements ContestSubmission
 	static ContestEvaluation evaluation_; 						//TODO Deze heb ik zelf static gemaakt om een error op te lossen; misschien is dat niet de way to go
     	private int evaluations_limit_;
 
-	private static final int GENERATION_LIMIT = 100000;				// TODO - default = 10000
+	private static final int GENERATION_LIMIT = 1000000;				// TODO - default = 10000
 	private static final int NUMBER_OF_INDIVIDUALS = 100; 				// size of population
-	private static final int CHILDREN_PER_GENERATION = 15;
+	private static final int CHILDREN_PER_GENERATION = 20;
 	private static final double RANDOM_MUTATION_PROB = 0.01;
 
 	private static final double tau = 1/Math.sqrt(10); 	// Learning rate, problem size (n) heb ik geinterpreteerd als dimensies van het fenotype (dat is de consensus ook op Slack op het moment)
@@ -66,6 +69,9 @@ public class player70 implements ContestSubmission
 		int generation = 0;
 		final Random randomNumbers = new Random();			// random number generator
 
+		// print to csv
+		System.out.println("Generation; Max; Min; Mean");
+
         	// init population
 		Individual[] currentPop = initPopulation(NUMBER_OF_INDIVIDUALS);
         	// calculate fitness - v1.1 fitness initiele populatie wordt nu berekend door externe evaluatie. 
@@ -79,7 +85,7 @@ public class player70 implements ContestSubmission
 
         	while(evals<evaluations_limit_ && generation < GENERATION_LIMIT){
 
-            		// Select parents		
+            		// Select parents	
 			int num_individuals = currentPop.length; 
 			Individual[] newParents = selectParents(currentPop);
 			int num_children = CHILDREN_PER_GENERATION;
@@ -98,14 +104,26 @@ public class player70 implements ContestSubmission
 			}  
 			Individual[] newPop = selectSurvivors(currentPop, newChildren);	
 			currentPop = newPop;
-			System.out.print("DIT WAS GENERATIE: ");
-			System.out.println(generation);
+			// System.out.print("DIT WAS GENERATIE: ");
+			// System.out.println(generation);
+			
+			double maxofgen = 0.0;
+			double minofgen = 10.0;
+			double mean = 0.0;
+			for (int i = 0; i < newPop.length; i++){
+				if (newPop[i].getFitness() > maxofgen) {
+					maxofgen = newPop[i].getFitness();
+				}
+				if (newPop[i].getFitness() < minofgen) {
+					minofgen = newPop[i].getFitness();
+				}
+				mean += newPop[i].getFitness();
+			}
+			mean = mean / newPop.length;
+			System.out.println(generation + "; " + maxofgen + "; " + minofgen + "; " + mean);
 			generation++;
-
 		}	// endwhile
- 		//DEBUG printPop(currentPop);
- 		// Double.parseDouble(System.getProperty("var1"));
-
+// }
 	}	// endrun()
 
 
@@ -280,10 +298,11 @@ public class player70 implements ContestSubmission
 
 	public static Individual[] selectSurvivors(Individual[] parentPop, Individual[] childrenPop) 
 	{
-		double[] fitnesslist = new double[NUMBER_OF_INDIVIDUALS + CHILDREN_PER_GENERATION]; // TODO - hier stond 120
-		//  int NUMBER_OF_INDIVIDUALS = 100; TODO - mag weg
+		double[] fitnesslist = new double[NUMBER_OF_INDIVIDUALS + CHILDREN_PER_GENERATION];
 		Individual[] newPop = new Individual[NUMBER_OF_INDIVIDUALS];
+		// int number_in_choicelist = 0;
 
+		// add all fitness scores to a list
 		for (int i = 0; i < (parentPop.length + childrenPop.length); i++) {
 			if (i < childrenPop.length) {
 				fitnesslist[i] = childrenPop[i].getFitness();
@@ -293,17 +312,19 @@ public class player70 implements ContestSubmission
 			}
 		}
 
+		// calculate length of roulettewheel list
 		int sumchoices = 0;
 		for (int i = 0; i < fitnesslist.length; i++) {
 			sumchoices += i;
-
 		}
+
+		// create roulettewheel list
 		double[] choicelist = new double[sumchoices];
 		int index = 0;
 
+		// fill roulettewheel list
 		Arrays.sort(fitnesslist);
 		for (int i = 0; i < fitnesslist.length; i++) {
-			// System.out.println(i + ": " +fitnesslist[i]);
 			for (int j = 0; j < i; j++) {
 				choicelist[index] = fitnesslist[i];
 				index += 1;
@@ -311,6 +332,7 @@ public class player70 implements ContestSubmission
 		}
 
 		for (int i = 0; i < NUMBER_OF_INDIVIDUALS; i++) {
+		
 			int random_ind = ThreadLocalRandom.current().nextInt(0, choicelist.length);
 			double searchedfitness = choicelist[random_ind];
 			while (searchedfitness == 0.0) {
@@ -331,31 +353,13 @@ public class player70 implements ContestSubmission
 					}
 				}
 			}
-			for (int k = 0; k < choicelist.length; k++){
+			
+			for (int k = 0; k < choicelist.length; k++) {
 				if (choicelist[k] == searchedfitness) {
 					choicelist[k] = 0.0;
 				}
 			}
-			// System.out.println("Lengte choicelist: " + choicelist.length);
 		}
-
-		// for (int i = 0; i < 100; i++) {
-		// 	double searchedfitness = fitnesslist[i + 20];
-		// 	for (int j = 0; j < 120; j++) {
-		// 		if (j < childrenPop.length) {
-		// 			if (childrenPop[j].getFitness() == searchedfitness) {
-		// 				newPop[i] = new Individual(childrenPop[j].getGenotype());
-		// 				newPop[i].setFitness(childrenPop[j].getFitness());
-		// 			}
-		// 		}
-		// 		else {
-		// 			if (parentPop[j - 20].getFitness() == searchedfitness) {
-		// 				newPop[i] = new Individual(parentPop[j - 20].getGenotype());
-		// 				newPop[i].setFitness(parentPop[j - 20].getFitness());
-		// 			}
-		// 		}
-		// 	}
-		// }
 
 		return newPop;
 	}
